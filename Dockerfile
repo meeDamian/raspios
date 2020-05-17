3d783ffb638948af3732bf52513b8fadca0c101f
Damian Mee <spam@meedamian.com>
2020-05-17 23:32:10 +0800

    stopped caring 22 commits ago

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,16 @@
-FROM debian:buster-slim AS debian-base
-RUN apt-get update && apt-get -y install wget gnupg unzip curl file
+FROM debian:buster-slim
+
+RUN apt-get update && \
+    apt-get -y install curl file gnupg unzip wget zip
 
 ADD modify-image.sh /usr/local/bin/modify-image
 RUN chmod +x /usr/local/bin/modify-image
 
-RUN mkdir -p /raspbian/ /mnt/raspbian/
-ADD firstboot.service /raspbian/
+RUN mkdir -p /data/ /mnt/raspbian/
+ADD firstboot.service /data/
 
 VOLUME /raspbian/
+WORKDIR /raspbian/
 
 ENTRYPOINT ["/usr/local/bin/modify-image"]
-CMD ["/raspbian/"]
+CMD ["magic", "/raspbian/"]
--- modify-image.sh
+++ modify-image.sh
@@ -51,13 +51,13 @@ download() {
 	log_ok
 }
 
-# Takes $file_name, and verifies consistency, and signature of image
+# Takes $original_zip, and verifies consistency, and signature of image
 verify() {
-	file_name="$1"
+	original_zip="$1"
 
 	log "Verifying image…"
 
-	if ! out="$(sha256sum -c "$file_name.sha256")"; then
+	if ! out="$(sha256sum -c "$original_zip.sha256")"; then
 		echo "Checksum doesn't match:" "$out"
 		return 1
 	fi
@@ -69,7 +69,7 @@ verify() {
 	fi
 	log_ok "GPG key fetched"
 
-	if ! gpg --verify "$file_name.sig"; then
+	if ! gpg --verify "$original_zip.sig"; then
 		echo "Signature verification failed"
 		return 1
 	fi
@@ -77,48 +77,111 @@ verify() {
 	log_ok "Valid signature"
 }
 
-mount_ext4() {
-	image_name="${1%.zip}.img"
+mount_image() {
+	original_image="$1"
 
-	log "Mounting ${image_name}…"
+	log "Mounting ${original_image}…"
 
-	startsector="$(file "$image_name" | grep -Eo 'startsector [[:digit:]]+' | cut -d' ' -f2 | sort -nr | head -n1)"
+	startsector="$(file "$original_image" | grep -Eo 'startsector [[:digit:]]+' | cut -d' ' -f2 | sort -nr | head -n1)"
 	if [ -z "$startsector" ]; then
-		log_err "Unable to find start sector of the last partition…"
+		echo "Unable to find start sector of the last partition…"
 		return 1
 	fi
 
 	log_ok "Found startsector: $startsector"
 
-	mkdir -p /mnt/
-	mount -o "loop,offset=$((startsector*512))" "$image_name" /mnt/raspbian/
-	ls -la /mnt/raspbian/
+	mount_dir=/mnt/raspbian/
+
+	mkdir -p "$mount_dir"
+	if ! out="$(mount -o "loop,offset=$((startsector*512))" "$original_image" "$mount_dir")"; then
+		echo "Unable to mount: $out"
+		return 1
+	fi
+
+	log_ok "$original_image mounted at $mount_dir"
 }
 
-all() {
-	dir="${1:-$(pwd)}"
+install_firstrun() {
+	file=firstboot.service
+	path=/mnt/raspbian/etc/systemd/system
+
+	log "Installing firstboot.service…"
+
+	cp "/data/$file" "$path/"
+	log_ok "Installed at ${path#/mnt/raspbian}/$file"
 
 	(
-		cd "$dir"
-
-		url="$(get_last_url)"
-		file_name="$(extract_filename "$url")"
-
-		download "$url"
-		if ! out="$(verify "$file_name")"; then
-			log_err "Verification failed: $out"
-			exit 1
-		fi
-
-		log "Inflating…"
-		unzip -n "$file_name"
-
-		mount_ext4 "$file_name"
+		cd "$path/multi-user.target.wants"
+		ln -s "/etc/systemd/system/$file" .
+		log_ok "Enabled as ${path#/mnt/raspbian}/multi-user.target.wants/$file"
 	)
 }
 
-if ! has_deps curl file gpg grep sha256sum unzip wget; then
-	exit 1
-fi
+all() {
+	url="$(get_last_url)"
+	original_zip="$(extract_filename "$url")"
 
-all "${1:-$(pwd)}"
+	download "$url"
+	if ! out="$(verify "$original_zip")"; then
+		log_err "Verification failed: $out"
+		exit 1
+	fi
+
+	original_image="${original_zip%.zip}.img"
+
+	log "Inflating…"
+	unzip -n "$original_zip"
+	log_ok "$original_zip unzipped into"
+	log_ok "$original_image"
+
+	if ! out="$(mount_image "$original_image")"; then
+		log_err "Mounting failed: $out"
+		exit 1
+	fi
+
+	install_firstrun
+
+	log "Unmounting /mnt/raspbian…"
+	umount /mnt/raspbian
+	log_ok
+
+	firstboot_image="${original_image%.img}-firstboot.img"
+
+	log "Renaming…"
+	mv "$original_image" "$firstboot_image"
+	log_ok "$original_image renamed to"
+	log_ok "$firstboot_image"
+
+	firstboot_zip="${firstboot_image%.img}.zip"
+	log "Deflating…"
+	zip "$firstboot_zip" "$firstboot_image"
+	log_ok "$firstboot_image zipped into"
+	log_ok "$firstboot_zip"
+
+	firstboot_checksum="$firstboot_zip.sha256"
+	log "Creating checksum file…"
+	sha256sum "$firstboot_zip" > "$firstboot_checksum"
+	log_ok "$(cat "$firstboot_checksum")"
+	log_ok "$firstboot_checksum"
+}
+
+
+case "$1" in
+version)
+  extract_version "$(get_last_url)"
+	exit 0
+	;;
+
+magic|*)
+	if ! has_deps curl file gpg grep sha256sum zip unzip wget; then
+		exit 1
+	fi
+
+	(
+		cd "${2:-$(pwd)}"
+		all
+	)
+	;;
+esac
+
+
